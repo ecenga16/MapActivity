4a2bdbfebbc118cc7ad455f8e39f44b9329b94c6
aldocenga <eraldocenga@nmc.al>
2023-10-12 16:24:00 +0200

    Add geolocation for user's location on the map

--- app.js
+++ app.js
@@ -13,16 +13,20 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
+        
         const {latitude} = position.coords;
         const {longitude} = position.coords;
 
-        const map = L.map('map').setView([51.505, -0.09], 13);
+        const coords = [latitude,longitude];
+        
+        const map = L.map('map').setView(coords, 13);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+        L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([51.5, -0.09])
+        L.marker(coords)
             .addTo(map)
             .bindPopup('A pretty CSS popup.<br> Easily customizable.')
             .openPopup();
